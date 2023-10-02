4d3d52b1fe40c51955a16b88c90db7c202201fb0
jpbarragan <jpbarragann@gmail.com>
2023-10-02 16:50:53 -0400

    Added dinamic content

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,19 +1,24 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
 public class HelloController {
 
     // handle request at path /hola
     @GetMapping("hola")
+    @ResponseBody
     public String hello (){
         return "Hello, Spring!";
     }
 
     @GetMapping("adios")
+    @ResponseBody
     public String goodbye (){
         return "Goodbye, Spring!";
 
@@ -21,28 +26,35 @@ public class HelloController {
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    public String helloWithQueryParameter(@RequestParam String name){
-        return "Hello, " + name +"!";
+    public String helloWithQueryParameter(@RequestParam String name, Model model){
+        String saludo = "Hello, " + name + "!";
+        model.addAttribute("greeting", saludo);
+        return "hello";
 
     }
 
     // Handles request of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    public String helloWithPathParameter(@PathVariable String name){
+    public String helloWithPathParameter(@PathVariable String name, Model model){
+        String saludo = "Hello," + name + "!";
+        model.addAttribute("greeting", saludo);
         return "Hello, " + name +"!";
     }
 
     // Handles requests using a Form
     @GetMapping("form")
-    @ResponseBody
     public String helloWithForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" +
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Dime algo!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
+    }
+
+    // Passes an iterating list of names
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Juan");
+        names.add("Pedro");
+        names.add("Jose");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 }
